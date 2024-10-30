9401e930499b6099d4184a6313dbbccdba0188bb
Tharshen2124 <tharshensurianbalan@gmail.com>
2024-10-30 22:33:40 +0800

    refactor: upload images to cloudinary and add the details in the database

--- SDKs/cloudinary.go
+++ SDKs/cloudinary.go
@@ -1,17 +1,21 @@
 package SDKs
 
 import (
-    "context"
+	"context"
+	"fmt"
 	"log"
-    "github.com/cloudinary/cloudinary-go/v2"
-    "github.com/cloudinary/cloudinary-go/v2/api"
-    "github.com/cloudinary/cloudinary-go/v2/api/admin"
-    "github.com/cloudinary/cloudinary-go/v2/api/uploader"
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
+	"github.com/google/uuid"
 )
 
+func GeneratePrefixedUniqueID() string {
+    id := uuid.New()
+    return fmt.Sprintf("img_%s", id.String()[:8])
+}
+
 func Credentials() (*cloudinary.Cloudinary, context.Context) {
-    // Add your Cloudinary credentials, set configuration parameter 
-    // Secure=true to return "https" URLs, and create a context
     cloudinaryInstance, _ := cloudinary.New()
     cloudinaryInstance.Config.URL.Secure = true
     ctx := context.Background()
@@ -19,8 +23,7 @@ func Credentials() (*cloudinary.Cloudinary, context.Context) {
 }
 
 func UploadImage(cloudinaryInstance *cloudinary.Cloudinary, ctx context.Context, image any, publicId string) {
-	// Upload image & set the asset's public ID and allow overwriting the asset with new versions
-	response, err := cloudinaryInstance.Upload.Upload(ctx, image, uploader.UploadParams{
+	_, err := cloudinaryInstance.Upload.Upload(ctx, image, uploader.UploadParams{
 		PublicID:      publicId,
 		UniqueFilename: api.Bool(false),
 		Overwrite:      api.Bool(true),
@@ -28,55 +31,22 @@ func UploadImage(cloudinaryInstance *cloudinary.Cloudinary, ctx context.Context,
 	
 	if err != nil {
 		log.Println("error")
-	}
-  	log.Println("2. Upload an image\nDelivery URL:", response.SecureURL, "\n")
-}
-
-func GetAssetInfo(cloudinaryInstance *cloudinary.Cloudinary, ctx context.Context, publicId string) {
-	// Get and use details of the image
-	response, err := cloudinaryInstance.Admin.Asset(ctx, admin.AssetParams{PublicID: publicId})
-	if err != nil {
-		log.Printf("error: %v", err)
-	}
-	log.Println("3. Get and use details of the image\nDetailed response:\n", response, "\n")
-
-	// Assign tags to the uploaded image based on its width. Save the response to the update in the variable 'update_resp'.
-	if response.Width > 900 {
-		update_resp, err := cloudinaryInstance.Admin.UpdateAsset(ctx, admin.UpdateAssetParams{
-			PublicID: publicId,
-			Tags:     []string{"large"}})
-		if err != nil {
-			log.Printf("error: %v", err)
-		} else {
-			log.Println("New tag: ", update_resp.Tags, "\n")
-		}
 	} else {
-		update_resp, err := cloudinaryInstance.Admin.UpdateAsset(ctx, admin.UpdateAssetParams{
-			PublicID: publicId,
-			Tags:     []string{"small"}})
-		if err != nil {
-			log.Printf("error: %v", err)
-		} else {
-			log.Println("New tag: ", update_resp.Tags, "\n")
-		}
+		log.Println("Image successfully uploaded")
 	}
 }
 
-func TransformImage(cloudinaryInstance *cloudinary.Cloudinary, ctx context.Context, publicId string) {
-    // Instantiate an object for the asset with public ID "my_image"
-    qs_img, err := cloudinaryInstance.Image(publicId)
+func GetTransformedImage(cloudinaryInstance *cloudinary.Cloudinary, ctx context.Context, publicId string) string {
+    image, err := cloudinaryInstance.Image(publicId)
     if err != nil {
         log.Println("error")
     }
 
-    // Add the transformation
-    qs_img.Transformation = "r_max/e_sepia"
-
-    // Generate and log the delivery URL
-    new_url, err := qs_img.String()
+    image.Transformation = "c_fill,g_auto,h_500,w_500"
+    transformedImageURL, err := image.String()
     if err != nil {
         log.Println("error")
-    } else {
-        print("4. Transform the image\nTransfrmation URL: ", new_url, "\n")
     }
+
+	return transformedImageURL
 }
